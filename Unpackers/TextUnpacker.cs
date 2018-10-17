f1d8ca89ab2fedeac6c281e688294c447116d35c
Jason Heuclin <jason@heuclin.com>
2018-10-17 19:19:31 +0100

    Text unpacker

---
+++ Unpackers/TextUnpacker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MediaEngine.Unpackers
+{
+    enum TextField
+    {
+        Colour = 16,
+        BackColour = 17,
+        Height = 18,
+        Size = 19,
+        Font = 21,
+        Byte22 = 22,
+        Strings = 32
+    }
+
+    class TextUnpacker : Unpacker<TextField>
+    {
+        protected override void Unpack(BinaryReader source, BinaryWriter destination, TextField field)
+        {
+            string value;
+
+            switch (field)
+            {
+                case TextField.Colour:
+                case TextField.BackColour:
+                    value = string.Join(", ", source.ReadByte(), source.ReadByte(), source.ReadByte());
+                    source.ReadByte();
+                    break;
+
+                case TextField.Font:
+                    value = Encoding.GetEncoding(932).GetString(source.ReadBytes(source.ReadInt32()));
+                    break;
+
+                case TextField.Byte22:
+                    value = source.ReadByte().ToString();
+                    break;
+
+                case TextField.Strings:
+                    var count = source.ReadInt32();
+                    value = string.Empty;
+                    for (int i = 0; i < count; i++)
+                        value += Environment.NewLine +
+                            Encoding.GetEncoding(932).GetString(source.ReadBytes(source.ReadInt32()));
+                    break;
+
+                default:
+                    value = source.ReadInt32().ToString();
+                    break;
+            }
+
+            destination.Write(Encoding.UTF8.GetBytes(string.Format("{0} = {1}\r\n", field, value)));
+        }
+    }
+}
