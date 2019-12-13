21e3a004f53b095cdf20fc0224ea9cfa20a41e6a
Jason Heuclin <jason@heuclin.com>
2019-12-13 18:44:06 +0000

    File dialog for inflation

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MediaEngine.Unpackers;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace MediaEngine
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Unpacked runtime (*.MXRU;*.LCRU;*.LCLU)|*.MXRU;*.LCRU;*.LCLU" +
+                    "|Packed runtime (*.MXR;*.LCR;*.LCL)|*.MXR;*.LCR;*.LCL"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                switch (Path.GetExtension(dialog.FileName)?.ToUpper())
+                {
+                    case ".MXR":
+                    case ".LCR":
+                    case ".LCL":
+                        Inflater.Inflate(dialog.FileName);
+                        break;
+                }
+            }
+
+            Shutdown();
+        }
+    }
+}
--- Unpackers/Inflater.cs
+++ Unpackers/Inflater.cs
@@ -1,5 +1,6 @@
 ﻿using Ionic.Zlib;
 using System.IO;
+using System.Linq;
 
 namespace MediaEngine.Unpackers
 {
@@ -7,13 +8,20 @@ namespace MediaEngine.Unpackers
     {
         public static void Inflate(string path)
         {
-            long offset = 28;
             using (var stream = File.OpenRead(path))
+            using (var reader = new BinaryReader(stream))
+            {
+                stream.Position = 23;
+
                 for (int i = 0; i < 2; i++)
                 {
-                    stream.Position = offset;
+                    var headerType = reader.ReadByte();
+                    var header = reader.ReadBytes(headerType == 240 ? 4 : 0);
 
-                    using (var output = File.Create(Path.ChangeExtension(path, null) + i + ".bin"))
+                    var lengthBytes = reader.ReadBytes(4).Select(b => (uint)b).ToArray();
+                    var length = lengthBytes[1] | (lengthBytes[2] << 8) | (lengthBytes[3] << 16) | ((lengthBytes[0] & 0x7F) << 24);
+
+                    using (var output = File.Create(Path.ChangeExtension(path, null) + " " + i + Path.GetExtension(path) + "u"))
                     using (var destination = new BinaryWriter(output))
                     using (var deflate = new ZlibStream(stream, CompressionMode.Decompress, true))
                     using (var source = new BinaryReader(deflate))
@@ -22,13 +30,14 @@ namespace MediaEngine.Unpackers
                             var bytes = source.ReadBytes(1024);
                             if (bytes.Length == 0)
                             {
-                                offset += deflate.TotalIn + 5;
+                                stream.Position = 28 + deflate.TotalIn;
                                 break;
                             }
 
                             destination.Write(bytes);
                         }
                 }
+            }
         }
     }
 }
